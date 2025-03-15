116b61bcde8016aa165a5e1a3d00fba34f0b2d10
ykoshevenko <ykoshevenko@list.ru>
2025-03-15 16:28:21 +0300

    финальный патч

--- vue.config.cjs
+++ vue.config.cjs
@@ -1,47 +1,12 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // Добавьте этот импорт
 
 module.exports = {
   configureWebpack: {
     plugins: [
       new MiniCssExtractPlugin({
         filename: '[name].css',
-        chunkFilename: '[id].css',
-      }),
-    ],
-  },
-  chainWebpack: config => {
-    config.module
-      .rule('css')
-      .uses.clear(); // Очищаем существующие правила для CSS
-
-    config.module
-      .rule('css')
-      .test(/\.css$/)
-      .use('style-loader')
-      .loader('style-loader'); // Добавляем style-loader
-
-    config.module
-      .rule('css')
-      .use('css-loader')
-      .loader('css-loader') // Добавляем css-loader
-      .options({
-        modules: true, // Если вы используете CSS-модули
-        sourceMap: true,
-      });
-
-    config.plugins.delete('extract-css'); // Удаляем встроенный экстрактор CSS
-  },
-};
-
-module.exports = {
-  configureWebpack: {
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-        },
-      ],
-    },
-  },
-};
+        chunkFilename: '[id].css'
+      })
+    ]
+  }
+}
